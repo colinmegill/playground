26ddac133168415a447d70face16c5f0a236ab75
Colin Megill <colinmegill@gmail.com>
2014-06-01 20:12:42 -0700

    creating a form, cleaning up dead play code, resourcejs file names changed and made into a variable added to global scope

--- resources.js
+++ resources.js
@@ -1,30 +1,35 @@
+window.resources = [
+
 {
-	resourceName: "The Attic Learning Community";
-	location: "Woodinville, Washington";
-	description: "The Attic offers a unique educational experience for children ages 3 -18, one which supports a sense of self, a passion for learning, community, and  childhood through our K-12 program and the Field & Forest Outdoor Preschool program. Many educative practices go into creating our progressive educative modelWe offer a low child:teacher ratio in a multiage environment. Our teachers are passionate, committed to each and every child. We understand the importance of freedom within structure, and of developmentally-appropriate education. Core classes meet three days per week, Monday through Wednesday, to give children and their families time to pursue their interests.";
-	link: "http://the-attic.org";
+	name: "The Attic Learning Community",
+	address: "Woodinville, Washington",
+	description: "The Attic offers a unique educational experience for children ages 3 -18, one which supports a sense of self, a passion for learning, community, and  childhood through our K-12 program and the Field & Forest Outdoor Preschool program. Many educative practices go into creating our progressive educative modelWe offer a low child:teacher ratio in a multiage environment. Our teachers are passionate, committed to each and every child. We understand the importance of freedom within structure, and of developmentally-appropriate education. Core classes meet three days per week, Monday through Wednesday, to give children and their families time to pursue their interests.",
+	url: "http://the-attic.org",
 	tags: ["program, outdoors/nature, arts, academic, social"]
-}
+},
 
 {
-	resourceName: "Frog Hollow";
-	location: "Seattle, Washington";
-	description: "Frog Hollow School is a one day a week writing program for homeschoolers. The elementary age curriculum integrates arts, music, social studies, mythology, and ecology into a solid core of language arts.  Through a mix of writing, art projects,  stories, singing, creative play, and outdoor exploration, students receive a firm foundation in creative writing, rhetoric, grammar, spelling, and literature.  As they explore written language from many angles, they develop their own sense of themselves as writers. Part of each day is also spent at the park playing and exploring nature.";
-	link: "http://froghollowschool.com/";
+	name: "Frog Hollow",
+	address: "Seattle, Washington",
+	description: "Frog Hollow School is a one day a week writing program for homeschoolers. The elementary age curriculum integrates arts, music, social studies, mythology, and ecology into a solid core of language arts.  Through a mix of writing, art projects,  stories, singing, creative play, and outdoor exploration, students receive a firm foundation in creative writing, rhetoric, grammar, spelling, and literature.  As they explore written language from many angles, they develop their own sense of themselves as writers. Part of each day is also spent at the park playing and exploring nature.",
+	url: "http://froghollowschool.com/",
 	tags: ["program, parent education, outdoors/nature, arts, academic, social, field trips"]
-}
+},
 
 {
-	resourceName: "School of Rock";
-	location: "Seattle, Washington";
-	description: "Through our performance-based approach to music lessons, School of Rock students are more inspired to learn, more motivated to excel and more confident as a result. We teach guitar, bass guitar, keyboards, drums and vocals in an interactive environment."
-	link: "http://www.schoolofrock.com/";
+	name: "School of Rock",
+	address: "Seattle, Washington",
+	description: "Through our performance-based approach to music lessons, School of Rock students are more inspired to learn, more motivated to excel and more confident as a result. We teach guitar, bass guitar, keyboards, drums and vocals in an interactive environment.",
+	url: "http://www.schoolofrock.com/",
 	tags: ["arts"]
-}
+},
 
 {
-	resourceName: "What Your Kindergartener Needs to Know";
-	description: "Designed for use by parents and teachers, this groundbreaking first volume in the Core Knowledge Series provides kindergartners with the fundamentals they need to prepare them for a lifetime of learning. It sets out the elements a parent or educator should look for in a good kindergarten program and introduces activities that help a child take the first steps in learning to read and write. Featuring a new Introduction and filled with age-appropriate questions and suggestions that stimulate thinking and build vocabulary, this revised and updated edition of What Your Kindergartner Needs to Know also includes.";
-	link: "http://www.randomhouse.com/book/80421/what-your-kindergartner-needs-to-know-revised-and-updated-by-ed-hirsch-jr";
-	tags: ["parent education"];
+	name: "What Your Kindergartener Needs to Know",
+	description: "Designed for use by parents and teachers, this groundbreaking first volume in the Core Knowledge Series provides kindergartners with the fundamentals they need to prepare them for a lifetime of learning. It sets out the elements a parent or educator should look for in a good kindergarten program and introduces activities that help a child take the first steps in learning to read and write. Featuring a new Introduction and filled with age-appropriate questions and suggestions that stimulate thinking and build vocabulary, this revised and updated edition of What Your Kindergartner Needs to Know also includes.",
+	url: "http://www.randomhouse.com/book/80421/what-your-kindergartner-needs-to-know-revised-and-updated-by-ed-hirsch-jr",
+	tags: ["parent education"],
 }
+
+
+]
