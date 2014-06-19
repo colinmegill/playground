7daebafa6528e46331b393df4787035a7c66f4a7
Christie <christiemegill@gmail.com>
2014-06-19 15:42:29 -0700

    underscore js for getting tags into right format from server

--- resources.js
+++ resources.js
@@ -1,12 +1,38 @@
-<script type="text/javascript">
 
 $(document).ready(function(){
 
-  $.get('resources').done(function(data){ 
+  $.get('resources').done(function(resources){ 
 
-    for (i=0; i<data.length; i++) {
-      $("#resources").append("<div class=\"col-md-4 \"> <div class=\"resource book\"> <button class=\"pull-right btn btn-sm btn-primary\"><i class=\"fa fa-plus\"></i></button> <h3><i class=\"fa fa-book\"></i>&nbsp;" + data[i].name +"</h3> <p><em>"+data[i].description+"</em></p> <p><a href="+ data[i].url +"><i class=\"fa fa-external-link\"></i>&nbsp;"+ data[i].name +"</a></p> </div> </div>")
-    }
+
+    var tags = [];
+
+
+    resources.forEach(function(resource){ 
+     tags.push(resource.tags)
+    })
+    tags = _.flatten(tags)
+    tags = _.uniq(tags)
+    console.log(tags)
+
+    tags.forEach(function(tag) {
+      $("#tags").append("<div ><button class=\"btn btn-xs btn-primary tagSortButton\">"+tag+"</button></div>")
+    })
+
+    $(".tagSortButton").click(function(){
+      $("#resources").html("")
+
+      //sort the collection, ie., only render the ones with the tag:
+      $(this).text()
+
+    // for (i=0; i<resources.length; i++) {
+    //    $("#resources").append("<div class=\"col-md-6 \"> <div class=\"resource book\"> <button class=\"pull-right btn btn-sm btn-primary\"><i class=\"fa fa-plus\"></i></button> <h3><i class=\"fa fa-book\"></i>&nbsp;" + resources[i].name +"</h3> <p><em>"+resources[i].description+"</em></p> <p><a href="+ resources[i].url +"><i class=\"fa fa-external-link\"></i>&nbsp;"+ resources[i].name +"</a></p> </div> </div>")
+    //  }
+    })
+
+
+    for (i=0; i<resources.length; i++) {
+       $("#resources").append("<div class=\"col-md-6 \"> <div class=\"resource book\"> <button class=\"pull-right btn btn-sm btn-primary\"><i class=\"fa fa-plus\"></i></button> <h3><i class=\"fa fa-book\"></i>&nbsp;" + resources[i].name +"</h3> <p><em>"+resources[i].description+"</em></p> <p><a href="+ resources[i].url +"><i class=\"fa fa-external-link\"></i>&nbsp;"+ resources[i].name +"</a></p> </div> </div>")
+     }
 
   })
 
@@ -43,4 +69,3 @@ $(document).ready(function(){
 })
 
 
-</script>
