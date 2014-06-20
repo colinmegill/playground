948b916ee1ec6ad615b149d895fd3197a8b3bb0f
Colin Megill <colinmegill@gmail.com>
2014-06-19 22:07:28 -0700

    adding masonry, stripe button, header layout

--- resources.js
+++ resources.js
@@ -3,28 +3,20 @@ $(document).ready(function(){
 
   $.get('resources').done(function(resources){ 
 
-
     var tags = [];
-
 
     resources.forEach(function(resource){ 
      tags.push(resource.tags)
     })
     tags = _.flatten(tags)
     tags = _.uniq(tags)
-    console.log(tags)
 
     tags.forEach(function(tag) {
       $("#tags").append("<div ><button class=\"btn btn-xs btn-primary tagSortButton\">"+tag+"</button></div>")
     })
-
     $(".tagSortButton").click(function(){
-      
       var $text = $(this).text()
-      $("#resources").html("<h3> you just clicked " + $text + " </h3>" )
-
       //sort the collection, ie., only render the ones with the tag:
-      
       for (i=0; i<resources.length; i++) {
         /*
           if the tags attribute, of the current model - which is an array - DOES NOT HAVE the
@@ -35,21 +27,22 @@ $(document).ready(function(){
           which it will because it is truthy.
         */
         if (resources[i].tags.indexOf($text) != -1 && resources[i].approved === true) {
-          $("#resources").append("<div class=\"col-md-6 \"> <div class=\"resource book\"> <button class=\"pull-right btn btn-sm btn-primary\"><i class=\"fa fa-plus\"></i></button> <h3><i class=\"fa fa-book\"></i>&nbsp;" + resources[i].name +"</h3> <p><em>"+resources[i].description+"</em></p> <p><a href="+ resources[i].url +"><i class=\"fa fa-external-link\"></i>&nbsp;"+ resources[i].name +"</a></p> </div> </div>")
+          $("#resources").append("<div class=\"col-md-6 masonry-item\"> <div class=\"resource book\"> <button class=\"pull-right btn btn-sm btn-primary\"><i class=\"fa fa-plus\"></i></button> <h3><i class=\"fa fa-book\"></i>&nbsp;" + resources[i].name +"</h3> <p><em>"+resources[i].description+"</em></p> <p><a href="+ resources[i].url +"><i class=\"fa fa-external-link\"></i>&nbsp;"+ resources[i].name +"</a></p> </div> </div>")
         }
       }
     })
-
-
     for (i=0; i<resources.length; i++) {
       if (resources[i].approved === true){
-        $("#resources").append("<div class=\"col-md-6 \"> <div class=\"resource book\"> <button class=\"pull-right btn btn-sm btn-primary\"><i class=\"fa fa-plus\"></i></button> <h3><i class=\"fa fa-book\"></i>&nbsp;" + resources[i].name +"</h3> <p><em>"+resources[i].description+"</em></p> <p><a href="+ resources[i].url +"><i class=\"fa fa-external-link\"></i>&nbsp;"+ resources[i].name +"</a></p> </div> </div>")
-      
+        $("#resources").append("<div class=\"col-md-6 masonry-item \"> <div class=\"resource book\"> <button class=\"pull-right btn btn-sm btn-primary\"><i class=\"fa fa-plus\"></i></button> <h3><i class=\"fa fa-book\"></i>&nbsp;" + resources[i].name +"</h3> <p><em>"+resources[i].description+"</em></p> <p><a href="+ resources[i].url +"><i class=\"fa fa-external-link\"></i>&nbsp;"+ resources[i].name +"</a></p> </div> </div>")
       }
     }
+    var container = document.querySelector('#resources');
+    var msnry = new Masonry( container, {
+      itemSelector: '.masonry-item',
+    });
+
 
   })
-
 
   $("#submitResource").click(function(e){
     e.preventDefault()
